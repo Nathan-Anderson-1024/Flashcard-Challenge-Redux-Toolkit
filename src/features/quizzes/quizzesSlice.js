dc5e347fed777f9a974abbbc37c3e4317c2b23cc
Naa003 <Nathan@scicrunch.com>
2023-01-10 11:20:44 -0800

    created thunk for adding quiz and associating to topic

--- src/features/quizzes/quizzesSlice.js
+++ src/features/quizzes/quizzesSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import { addQuizIds } from "../topics/topicsSlice";
 export const quizzesSlice = createSlice({
     name: 'quizzes',
     initialState: {
@@ -9,16 +9,20 @@ export const quizzesSlice = createSlice({
     },
     reducers: {
         addQuiz: (state, action) => {
-            const {id, name, topicId, cardIds} = action.payload;
-            state.quizzes[id] = {
-                id: id,
-                name: name,
-                topicId: topicId,
-                cardIds: cardIds
-            }
+            const { id } = action.payload;
+            state.quizzes[id] = action.payload
         }
     }
-})
+});
+
+export const thunkCombineProcess = (quiz) => {
+    const { topicId, id } = quiz;
+    return (dispatch) => {
+        dispatch(quizzesSlice.actions.addQuiz(quiz));
+        dispatch(addQuizIds({topicId: topicId, quizId: id}))
+    }
+};
+
 export default quizzesSlice.reducer
 export const selectQuizzes = (state) => state.quizzes.quizzes;
 export const {addQuiz} = quizzesSlice.actions
