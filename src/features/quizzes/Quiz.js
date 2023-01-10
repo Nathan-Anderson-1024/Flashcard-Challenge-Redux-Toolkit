85acc4458aa71d66dc96c9c0b0791f79958a9467
Naa003 <Nathan@scicrunch.com>
2023-01-10 12:12:15 -0800

    can select cards in cards.js

--- src/features/quizzes/Quiz.js
+++ src/features/quizzes/Quiz.js
@@ -2,8 +2,7 @@ import { Link, useParams } from "react-router-dom";
 import Card from "../cards/Card";
 import ROUTES from "../../app/routes";
 import { selectQuizzes } from "./quizzesSlice";
-import { useSelector
- } from "react-redux";
+import { useSelector } from "react-redux";
 export default function Topic() {
   const quizzes = useSelector(selectQuizzes); // replace this with a call to your selector to get all the quizzes in state
   let { quizId } = useParams();
