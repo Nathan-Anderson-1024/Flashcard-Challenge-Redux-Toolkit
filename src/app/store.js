8b78e4ce93562cd64528f97b13c8e76186646c04
Naa003 <Nathan@scicrunch.com>
2023-01-10 10:58:21 -0800

    initial commit, created topics slice

---
+++ src/app/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import topicsReducer from '../features/topics/topicsSlice.js'
+export default configureStore({
+  reducer: {
+    topics: topicsReducer
+  },
+});
